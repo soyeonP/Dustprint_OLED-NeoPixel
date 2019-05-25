fd18b00686a2ee97addcdf27e82b81d2109929bc
soyeonP <uyyu173@naver.com>
2019-05-25 22:52:36 +0900

    neopixel정보 추가/

--- Dust_printin_OLED/Dust_printin_OLED.ino
+++ Dust_printin_OLED/Dust_printin_OLED.ino
@@ -1,3 +1,7 @@
+/*
+ * 본 저작물은 '한국환경공단'에서 실시간 제공하는 '한국환경공단_대기오염정보'를 이용하였습니다.
+ * https://www.data.go.kr/dataset/15000581/openapi.do
+ */
 //oled정의
 #include <SPI.h>
 #include <Wire.h>
@@ -6,6 +10,9 @@
 //wifi정의
 #include <ESP8266WiFi.h> // ESP 8266 와이파이 라이브러리
 #include <ESP8266HTTPClient.h> // HTTP 클라이언트
+#include <Adafruit_NeoPixel.h> // 네오픽셀 라이브러리
+#define NUMPIXELS      19 // 네오픽셀 LED 수
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, D0, NEO_GRB + NEO_KHZ800); // 네오픽셀 객체
 
 //oled정의
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
@@ -14,23 +21,21 @@
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
+#define NUMFLAKES     19 // Number of snowflakes in the animation example
 
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
 
-
 //wifi정의
 String city = "서울"; // 서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종 중 입력
 String gu = "용산구";
-String guForPrint = "YungsanGu";
 String key = "nP6NBdnElmAi%2BPRK%2B%2F%2FTMw%2Fq80%2B1vwK2cAck%2FiiTiwWARg4ZEEudXBEfL6a7rK7D0eSfXPRlQu5BLPemPOqiYw%3D%3D";
 String url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?sidoName=" + city + "&searchCondition=HOUR&pageNo=1&numOfRows=200&ServiceKey=" + key;
 float pm10,pm25 = 0 ; 
 int score = 0 ;
 
 //출력할 문자
-String text_display = "scroll";
+String text_display = "Dust value";
 
 static const unsigned char PROGMEM logo_bmp[] =
 { B00000000, B11000000,
@@ -55,6 +60,14 @@ void setup()
   // 시리얼 세팅
   Serial.begin(115200);
   Serial.println();
+  
+ // 네오픽셀 초기화
+   #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+ pixels.begin();
+  pixels.show(); 
 
 //와이파이 정보 
 #ifndef STASSID
@@ -112,6 +125,7 @@ void setup()
   delay(1000);  
 }
 
+
 void loop() {
   if (WiFi.status() == WL_CONNECTED) // 와이파이가 접속되어 있는 경우
   {
@@ -130,6 +144,9 @@ void loop() {
           pm10 = getNumber(payload, "<pm10Value>", cityIndex); 
           pm25 =getNumber(payload,"<pm25Value>", cityIndex);
           //Serial.println(payload);
+          Serial.println("용산구");
+          Serial.println( pm10);
+          Serial.println( pm25 );
         }
       } else {
         Serial.printf("[HTTP] GET... 실패, 에러코드: %s\n", http.errorToString(httpCode).c_str());
@@ -141,8 +158,12 @@ void loop() {
     testscrolltext();
     Serial.println(gu +"의 미세먼지 수치");
     Serial.println(pm10);
-    Serial.println(pm25);
-    delay(600000);
+    Serial.println(pm25);  
+    //네오픽셀 출력 // 
+    score = getScore(); // score 변수에 대기오염점수 저장
+    Serial.println(score); // 시리얼로 출력
+    setLEDColor(score); // 점수에 따라 LED 색상 출력
+    delay(60000);    
   }
 }
 
@@ -155,15 +176,54 @@ float getNumber(String str, String tag, int from) {
   return s.toFloat();
 }
 
+//미세먼지 수치  // 
+int getScore() {
+  int s = -1;
+  if(pm10 > 151 || pm25>=76) //4.최악
+   s = 4;
+  else if (pm10 >= 101 || pm25 >= 51 ) // 3.매우 나쁨
+    s = 3;
+  else if (pm10 >= 51 || pm25 >= 26) // 2.나쁨
+    s = 2;
+  else if (pm10 >= 41 || pm25 >= 21 ) //1. 보통
+    s = 1;
+  else  // 0.양호
+    s = 0;
+    
+  return s;
+}
+
+void setLEDColor(int s) {
+  int color;
+  if (s == 0) // 양호 진파랑
+      colorWipe(pixels.Color(0, 63, 255),30);
+  else if (s == 1) // 보통 연초록
+      colorWipe(pixels.Color(0, 127, 63),30);
+  else if (s == 2) // 나쁨 주황
+      colorWipe(pixels.Color(255, 127, 0),30);
+  else if (s == 3) // 매우 나쁨 연빨강 
+    colorWipe(pixels.Color(255, 31, 0),30);
+   else  //최악 빨강
+   colorWipe(pixels.Color(255, 0, 0),30);
+
+}
+
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<NUMPIXELS ; i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
+}
 
 void testscrolltext(void) {
-
   display.clearDisplay();
-  printStringSet() ; // gu의 문자값 영어로 변경 출력
+
   display.setTextSize(2); // Draw 2X-scale text
   display.setTextColor(WHITE);
   display.setCursor(10, 0);
-  display.println(( guForPrint + pm10 + " " + pm25 ));
+  display.println(( String("yongsanGu") + pm10 + " " + pm25 ));
   display.display();      // Show initial text
   delay(100);
 
@@ -172,57 +232,4 @@ void testscrolltext(void) {
   delay(1000);
 }
 
-void printStringSet (){  // gu의 문자값 영어로 변경 출력
-  
-  switch (gu) {
-  case '용산구':
-  guForPrint = "Yungsan-gu";
-  break;
-  case '강서구':
-  guForPrint = "Gangsu-gu";
-  break;
-  case '양천구':
-  guForPrint = "Youngcheon-gu";
-  break;
-  case '구로구':
-  guForPrint = "Guro-gu";
-  break;
-  case '금천구':
-  guForPrint = "Gmcheon-gu";
-  break;
-  case '마포구':
-  guForPrint = "Mapo-gu";
-  break;
-  case '영등포구':
-  guForPrint = "Yougdungpo-gu";
-  break;
-  case '동작구':
-  guForPrint = "Dongjak-gu";
-  break;
-    case '관악구':
-  guForPrint = "Ganak-gu";
-  break;
-    case '은평구':
-  guForPrint = "Enpeong-gu";
-  break;
-    case '종로구':
-  guForPrint = "Jongro-gu";
-  break;
-    case '서대문구':
-  guForPrint = "Seodaemon-gu";
-  break;
-    case '서초구':
-  guForPrint = "Seocho-gu";
-  break;
-    case '중구':
-  guForPrint = "Jung-gu";
-  break;
-    case '강남구':
-  guForPrint = "gangnam-gu";
-  break;
-    case '성동구':
-  guForPrint = "Seongdong-gu";
-  break;
-  }
-  }
 
